b584dc4cd8ec498b7cf728d72ab8a3d799ccf198
Lucia <lucia.matest@educa.jcyl.es>
2023-03-01 22:59:32 +0000

    1

--- Diseño/userDetalles.php
+++ Diseño/userDetalles.php
@@ -3,3 +3,109 @@
 <link rel="stylesheet" href="css/detallesStyle.css">
 <link rel="stylesheet" href="css/tipografia.css">
 
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th class="d-none d-md-table-cell">ID</th>
+                <th>Fecha</th>
+                <th>Temperatura</th>
+                <th class="d-none d-md-table-cell">Humedad</th>
+                <th class="d-none d-md-table-cell">Luminosidad</th>
+                <th class="d-none d-md-table-cell">Personas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="d-none d-md-table-cell">1</td>
+                <td class="d-block d-md-none">
+                    <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                        <span class="ms-1 d-none d-sm-inline">01/03/2023 02:55:12</span> </a>
+                    <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                        <li class="w-100">
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                        </li>
+                    </ul>
+                </td>
+                <td class="d-none d-md-table-cell">01/03/2023 02:55:12</td>
+                <td>25º</td>
+                <td class="d-none d-md-table-cell">10%</td>
+                <td class="d-none d-md-table-cell">200</td>
+                <td class="d-none d-md-table-cell">5</td>
+            </tr>
+            <tr>
+                <td class="d-none d-md-table-cell">1</td>
+                <td class="d-block d-md-none">
+                    <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                        <span class="ms-1 d-none d-sm-inline">01/03/2023 02:55:12</span> </a>
+                    <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                        <li class="w-100">
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                        </li>
+                    </ul>
+                </td>
+                <td class="d-none d-md-table-cell">01/03/2023 02:55:12</td>
+                <td>25º</td>
+                <td class="d-none d-md-table-cell">10%</td>
+                <td class="d-none d-md-table-cell">200</td>
+                <td class="d-none d-md-table-cell">5</td>
+            </tr>
+            <tr>
+                <td class="d-none d-md-table-cell">1</td>
+                <td class="d-block d-md-none">
+                    <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                        <span class="ms-1 d-none d-sm-inline">01/03/2023 02:55:12</span> </a>
+                    <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                        <li class="w-100">
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                        </li>
+                    </ul>
+                </td>
+                <td class="d-none d-md-table-cell">01/03/2023 02:55:12</td>
+                <td>25º</td>
+                <td class="d-none d-md-table-cell">10%</td>
+                <td class="d-none d-md-table-cell">200</td>
+                <td class="d-none d-md-table-cell">5</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
